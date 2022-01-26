c12c978612ca1da3e380c416389fa05ee39c686f
Marshall Hahn <git@marshallhahn.com>
2022-01-25 21:35:00 -0600

    create method and view

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,4 +2,8 @@ class BlogPostsController < ApplicationController
     def new
         @blog_post = BlogPost.new
     end
+
+    def create
+        @blog_post = BlogPost.new(params[:blog_post])
+    end
 end
