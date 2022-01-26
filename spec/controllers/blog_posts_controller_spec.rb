9a7a266fa406704497575fee2e99f8f7d029c9f1
Marshall Hahn <git@marshallhahn.com>
2022-01-25 21:32:58 -0600

    Resolve issues with acceptance test and form syntax

--- spec/controllers/blog_posts_controller_spec.rb
+++ spec/controllers/blog_posts_controller_spec.rb
@@ -4,9 +4,9 @@ describe BlogPostsController do
     describe '#new' do
         it 'returns a blog post' do
             blog_post = instance_double(BlogPost)
-            expect(BlogPost).to_receive(:new).and_return(blog_post)
+            expect(BlogPost).to receive(:new).and_return(blog_post)
             get :new
-            expect(assigns[:blog_post]).to_eq(blog_post)
+            expect(assigns[:blog_post]).to eq(blog_post)
         end
     end
 end
